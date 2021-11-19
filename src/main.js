61e2c8f2ef42d8692ba063171dec99fc240e74c5
juliadud <yuliiadudkina@gmail.com>
2021-11-19 13:31:35 +0200

    adding ui components

--- src/main.js
+++ src/main.js
@@ -2,9 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from  './router';
 import store from "./store";
+import Wrapper from './components/UI/Wrapper';
+import LinkButton from "./components/UI/LinkButton";
 
 const app = createApp(App);
 app.use(router);
 app.use(store);
+app.component('wrapper', Wrapper);
+app.component('link-button', LinkButton);
 
 app.mount('#app');
