7074be1b386cadc79755aeec757b02f171543759
juliadud <yuliiadudkina@gmail.com>
2021-11-22 17:10:43 +0200

    disabling links if not authorized

--- src/components/MainList.vue
+++ src/components/MainList.vue
@@ -3,31 +3,31 @@
     <wrapper>
       <li>
         <h2>Clients</h2>
-        <link-button mode="flat" link to="/clients">Edit</link-button>
+        <link-button :class="{disabled: !isAuthenticated}" mode="flat" link to="/clients">Edit</link-button>
       </li>
     </wrapper>
     <wrapper>
       <li>
         <h2>Worksites</h2>
-        <link-button mode="flat" link to="/worksites">Edit</link-button>
+        <link-button :class="{disabled: !isAuthenticated}" mode="flat" link to="/worksites">Edit</link-button>
         </li>
     </wrapper>
     <wrapper>
       <li>
         <h2>Employees</h2>
-        <link-button mode="flat" link to="/employees">Edit</link-button>
+        <link-button :class="{disabled: !isAuthenticated}" mode="flat" link to="/employees">Edit</link-button>
         </li>
     </wrapper>
     <wrapper>
       <li>
         <h2>Equipment</h2>
-        <link-button mode="flat" link to="/equipment">Edit</link-button>
+        <link-button :class="{disabled: !isAuthenticated}" mode="flat" link to="/equipment">Edit</link-button>
         </li>
     </wrapper>
     <wrapper>
       <li>
         <h2>Jobs</h2>
-        <link-button mode="flat" link to="/jobs">Edit</link-button>
+        <link-button :class="{disabled: !isAuthenticated}" mode="flat" link to="/jobs">Edit</link-button>
         </li>
     </wrapper>
   </ul>
@@ -37,7 +37,12 @@
 import Wrapper from "./UI/Wrapper";
 export default {
   name: "MainList",
-  components: {Wrapper}
+  components: {Wrapper},
+  computed:{
+    isAuthenticated(){
+      return this.$store.getters.isAuthenticated;
+    }
+  }
 }
 </script>
 
@@ -58,5 +63,8 @@ a{
   text-decoration: none;
   margin: 20px 0;
   font-size: 1.5em;
+}
+a.disabled {
+  pointer-events: none;
 }
 </style>
