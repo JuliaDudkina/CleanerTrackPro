1b9c45bb13376a6046d7d71d5175907eda62a82c
juliadud <yuliiadudkina@gmail.com>
2021-11-22 11:49:40 +0200

    importing employees module

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,13 @@
 import {createStore} from "vuex";
-import clients from './modules/clients/index'
+import clients from './modules/clients/index';
+import auth from './modules/auth/index';
+import employees from './modules/employees/index'
 
 const store = createStore({
     modules:{
         clients,
+        auth,
+        employees
     }
 })
 
