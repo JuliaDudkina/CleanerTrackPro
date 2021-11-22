70031799b735d19b3822c5954721f4a59b610e04
juliadud <yuliiadudkina@gmail.com>
2021-11-22 16:06:36 +0200

    displaying equipment list

---
+++ src/components/equipment/EquipmentDetails.vue
@@ -0,0 +1,37 @@
+<template>
+  <li >
+    <wrapper class="flex">
+      <div>
+        <h3>Name: {{name}}</h3>
+        <h3>Fee: ${{ fee }}</h3>
+        <h3>Storage Location: {{ storage }}</h3>
+      </div>
+      <div>
+<!--        <link-button @click="updateItem">Update</link-button>-->
+        <link-button @click="deleteItem">Delete</link-button>
+      </div>
+    </wrapper>
+  </li>
+</template>
+
+<script>
+
+export default {
+  name: "EquipmentDetails",
+  props:['id','name','fee', 'storage'],
+  methods:{
+    deleteItem(){
+      const itemId = this.id;
+      this.$store.dispatch('deleteEquipment', itemId)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.flex{
+  display: flex;
+  justify-content: space-between;
+  align-items: baseline;
+}
+</style>
---
+++ src/components/equipment/EquipmentList.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <wrapper class="header">
+      <h1>Equipment</h1>
+      <link-button link to="/equipment/register">Add Equipment</link-button>
+    </wrapper>
+    <ul>
+      <equipment-details
+          v-for="item in equipment"
+            :key="item.name"
+            :id="item.id"
+            :name="item.name"
+            :fee="item.fee"
+            :storage="item.storage"
+      >
+      </equipment-details>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Wrapper from "../UI/Wrapper";
+import EquipmentDetails from "./EquipmentDetails";
+export default {
+  name: "EquipmentList",
+  components: {Wrapper, EquipmentDetails},
+  computed:{
+    equipment(){
+      return this.$store.getters.equipment;
+    }
+  },
+  methods:{
+    loadEquipment(){
+      try {
+        this.$store.dispatch('loadEquipment');
+      } catch (error) {
+        this.error = error.message || 'Something went wrong!';
+      }
+    }
+  },
+  created() {
+    this.loadEquipment();
+  }
+}
+</script>
+
+<style scoped>
+.header{
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  box-shadow: none;
+}
+ul{
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+</style>
