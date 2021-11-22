51f590d856ddc644d1acb9198ab415aeb8843289
juliadud <yuliiadudkina@gmail.com>
2021-11-22 17:07:43 +0200

    adding worksite components

---
+++ src/components/worksites/AddWorksite.vue
@@ -0,0 +1,17 @@
+<template>
+  <wrapper>
+    <form>
+      <h2>Add a new worksite</h2>
+    </form>
+  </wrapper>
+</template>
+
+<script>
+export default {
+  name: "AddWorksite"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/worksites/WorksitesList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <wrapper class="header">
+      <h1>Worksites</h1>
+      <link-button link to="/worksites/register">Add a Worksite</link-button>
+    </wrapper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "WorksitesList"
+}
+</script>
+
+<style scoped>
+.header{
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  box-shadow: none;
+}
+</style>
