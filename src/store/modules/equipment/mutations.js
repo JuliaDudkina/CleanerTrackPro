24c8a85a637faf06be7dec4dc532b5365bb1dc86
juliadud <yuliiadudkina@gmail.com>
2021-11-22 16:05:49 +0200

    adding mutations for equipment

---
+++ src/store/modules/equipment/mutations.js
@@ -0,0 +1,12 @@
+export default {
+    addEquipment(state, payload) {
+        state.equipment.unshift(payload);
+    },
+    setEquipment(state,payload){
+        state.equipment = payload;
+    },
+    deleteEquipment(state, payload) {
+        const resIndex = state.equipment.findIndex( item => item.id === payload);
+        state.equipment.splice(resIndex,1);
+    }
+}
