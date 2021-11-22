37abe1c6db144087f2bc554293e7284e27f7c46d
juliadud <yuliiadudkina@gmail.com>
2021-11-22 11:49:00 +0200

    displaying list of employees

---
+++ src/components/employees/EmployeesDetails.vue
@@ -0,0 +1,47 @@
+<template>
+  <li>
+    <wrapper class="flex">
+      <div>
+        <div>
+          <h1>{{ name }}</h1>
+          <p>Phone: <a :href="'tel:' + phone">{{ phone}}</a></p>
+          <p>Salary: ${{ salary }}</p>
+          <p>Birth Date: {{birthDate}}</p>
+        </div>
+        <div class="inline">
+          <p>Address:</p>
+          <address> {{ address }}</address>
+        </div>
+      </div>
+      <div>
+        <link-button @click="updateClient">Update</link-button>
+        <link-button @click="deleteClient">Delete</link-button>
+      </div>
+    </wrapper>
+  </li>
+</template>
+
+<script>
+export default {
+  name: "EmployeesDetails",
+  props: ['id','name', 'phone', 'salary', 'birthDate', 'address'],
+  methods:{
+    deleteClient(){
+      const clientId = this.id;
+      this.$store.dispatch('deleteEmployee', clientId);
+    },
+  }
+}
+</script>
+
+<style scoped>
+.flex{
+  display: flex;
+  justify-content: space-around;
+  align-items: baseline;
+}
+.inline{
+  display: flex;
+  align-items: center;
+}
+</style>
---
+++ src/components/employees/EmployeesList.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <wrapper class="header">
+      <h1>Employees</h1>
+      <link-button link to="/employees/register"> Add Employee</link-button>
+    </wrapper>
+    <ul>
+      <employees-details
+          v-for="(employee) in employees"
+          :key="employee.id"
+          :id="employee.id"
+          :name="employee.name"
+          :phone="employee.phone"
+          :address="employee.address"
+          :birth-date="employee.birthDate"
+          :salary="employee.salary"
+      ></employees-details>
+    </ul>
+  </div>
+</template>
+
+<script>
+import EmployeesDetails from "./EmployeesDetails";
+export default {
+  name: "EmployeesList",
+  components: {EmployeesDetails},
+  computed: {
+    employees(){
+      return this.$store.getters.employees;
+    }
+  },
+  methods:{
+    async loadEmployees() {
+      try {
+        await this.$store.dispatch('loadEmployees');
+      } catch (error) {
+        this.error = error.message || 'Something went wrong!';
+      }
+    },
+  },
+  created(){
+      this.loadEmployees();
+  },
+}
+</script>
+
+<style scoped>
+ul{
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+.header{
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  box-shadow: none;
+}
+</style>
