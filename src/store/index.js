fc74b3e668fd58f3f53756ff6533ef5607735ea2
juliadud <yuliiadudkina@gmail.com>
2021-11-22 17:11:28 +0200

    importing equipment module

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,15 @@
 import {createStore} from "vuex";
 import clients from './modules/clients/index';
 import auth from './modules/auth/index';
-import employees from './modules/employees/index'
+import employees from './modules/employees/index';
+import equipment from './modules/equipment/index';
 
 const store = createStore({
     modules:{
         clients,
         auth,
-        employees
+        employees,
+        equipment
     }
 })
 
