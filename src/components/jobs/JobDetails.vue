429cca00b464da6ad66dc1fb2666f1a39d7fb6ba
juliadud <yuliiadudkina@gmail.com>
2021-11-22 17:08:02 +0200

    adding jobs components

---
+++ src/components/jobs/JobDetails.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "WorsiteDetails"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/jobs/JobsList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <wrapper class="header">
+      <h1>Jobs</h1>
+      <link-button link to="/jobs/register">Add a New Job</link-button>
+    </wrapper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "WorksiteList"
+}
+</script>
+
+<style scoped>
+.header{
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  box-shadow: none;
+}
+</style>
