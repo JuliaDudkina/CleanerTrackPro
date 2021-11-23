4dfb8472e850a45c609f96a043aa20e202d56dd7
juliadud <yuliiadudkina@gmail.com>
2021-11-23 15:26:12 +0200

    adding form to add a new worksite

--- src/components/worksites/AddWorksite.vue
+++ src/components/worksites/AddWorksite.vue
@@ -1,14 +1,27 @@
 <template>
-  <wrapper>
-    <form>
-      <h2>Add a new worksite</h2>
-    </form>
-  </wrapper>
+    <worksite-form  @action="saveData">
+      <template v-slot:title>
+        <h2>Add a new worksite</h2>
+      </template>
+      <template v-slot:button>
+        <link-button>Submit</link-button>
+      </template>
+    </worksite-form>
 </template>
 
 <script>
+import WorksiteForm from "./WorksiteForm";
 export default {
-  name: "AddWorksite"
+  name: "AddWorksite",
+  components:{ WorksiteForm },
+  methods:{
+    async saveData(data) {
+      const newWorksite = data;
+      this.$store.dispatch('addWorksite', newWorksite);
+      await this.$store.dispatch('loadWorksites');
+      this.$router.replace('/worksites');
+    }
+  }
 }
 </script>
 
