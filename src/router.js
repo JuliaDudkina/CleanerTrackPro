d7c11cc875458fce695a8de094d43908eebe1e6a
juliadud <yuliiadudkina@gmail.com>
2021-11-24 15:10:35 +0200

    adding navigation guards

--- src/router.js
+++ src/router.js
@@ -16,28 +16,39 @@ import UpdateEmployee from "./components/employees/UpdateEmployee";
 import UpdateEquipment from "./components/equipment/UpdateEquipment";
 import UpdateWorksite from "./components/worksites/UpdateWorksite";
 import UpdateJob from "./components/jobs/UpdateJob";
+import store from "./store";
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         {path: '/', component: MainList},
-        {path: '/clients', component: ClientsList},
-        {path: '/clients/register', component: AddClient},
-        {path: '/clients/update', component: UpdateClient},
-        {path: '/jobs', component: JobsList},
-        {path: '/jobs/register', component: AddJob},
-        {path: '/jobs/update', component: UpdateJob},
-        {path: '/employees', component: EmployeesList},
-        {path: '/employees/register', component: AddEmployee},
-        {path: '/employees/update', component: UpdateEmployee},
-        {path: '/equipment', component: EquipmentList},
-        {path: '/equipment/register', component: AddEquipment},
-        {path: '/equipment/update', component: UpdateEquipment},
-        {path: '/worksites', component: WorksitesList},
-        {path: '/worksites/register', component: AddWorksite},
-        {path: '/worksites/update', component: UpdateWorksite},
-        {path: '/auth', component: Auth},
+        {path: '/clients', component: ClientsList, meta:{requiresAuth: true}},
+        {path: '/clients/register', component: AddClient, meta:{requiresAuth: true}},
+        {path: '/clients/update', component: UpdateClient, meta:{requiresAuth: true}},
+        {path: '/jobs', component: JobsList, meta:{requiresAuth: true}},
+        {path: '/jobs/register', component: AddJob, meta:{requiresAuth: true}},
+        {path: '/jobs/update', component: UpdateJob, meta:{requiresAuth: true}},
+        {path: '/employees', component: EmployeesList, meta:{requiresAuth: true}},
+        {path: '/employees/register', component: AddEmployee, meta:{requiresAuth: true}},
+        {path: '/employees/update', component: UpdateEmployee, meta:{requiresAuth: true}},
+        {path: '/equipment', component: EquipmentList, meta:{requiresAuth: true}},
+        {path: '/equipment/register', component: AddEquipment, meta:{requiresAuth: true}},
+        {path: '/equipment/update', component: UpdateEquipment, meta:{requiresAuth: true}},
+        {path: '/worksites', component: WorksitesList, meta:{requiresAuth: true}},
+        {path: '/worksites/register', component: AddWorksite, meta:{requiresAuth: true}},
+        {path: '/worksites/update', component: UpdateWorksite, meta:{requiresAuth: true}},
+        {path: '/auth', component: Auth, meta:{requiresUnAuth: true}},
     ]
+});
+
+router.beforeEach(function (to,from,next){
+    if(to.meta.requiresAuth && !store.getters.isAuthenticated ){
+        next('/auth');
+    } else if(to.meta.requiresUnAuth && store.getters.isAuthenticated){
+        next('/');
+    } else {
+        next();
+    }
 });
 
 export default router;
