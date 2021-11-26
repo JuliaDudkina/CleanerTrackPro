41821741cb3c47b40e701ef48b442652008efb63
juliadud <yuliiadudkina@gmail.com>
2021-11-26 14:39:20 +0200

    adding success dialog to Add Worksite comp

--- src/components/worksites/AddWorksite.vue
+++ src/components/worksites/AddWorksite.vue
@@ -1,4 +1,9 @@
 <template>
+  <div>
+    <Dialog @close="closeDialog" :show="success" title="Success!">
+      <h3>A new worksite has been successfully created!</h3>
+      <template v-slot:buttonText>Go to Worksites' List</template>
+    </Dialog>
     <worksite-form  @action="saveData">
       <template v-slot:title>
         <h2>Add a new worksite</h2>
@@ -7,18 +12,28 @@
         <link-button>Submit</link-button>
       </template>
     </worksite-form>
+  </div>
 </template>
 
 <script>
 import WorksiteForm from "./WorksiteForm";
+import Dialog from "../UI/Dialog";
 export default {
   name: "AddWorksite",
-  components:{ WorksiteForm },
+  components:{ WorksiteForm, Dialog },
+  data(){
+    return{
+      success: false
+    }
+  },
   methods:{
     async saveData(data) {
+      this.success = true;
       const newWorksite = data;
       await this.$store.dispatch('addWorksite', newWorksite);
       await this.$store.dispatch('loadClientWorksites');
+    },
+    closeDialog(){
       this.$router.go(-1);
     }
   }
