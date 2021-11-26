6e839947badf1079866629269854e12fb13c1745
juliadud <yuliiadudkina@gmail.com>
2021-11-26 14:38:53 +0200

    adding success dialog to Update Worksite comp

--- src/components/worksites/UpdateWorksite.vue
+++ src/components/worksites/UpdateWorksite.vue
@@ -1,16 +1,22 @@
 <template>
-  <worksite-form @action="update"
-    :old-name="oldData.name"
-    :old-address="oldData.address"
-    :old-type="oldData.type"
-  >
-    <template v-slot:title>
-      <h2>Update the worksite</h2>
-    </template>
-    <template v-slot:button>
-      <link-button>Update</link-button>
-    </template>
-  </worksite-form>
+  <div>
+    <Dialog @close="closeDialog" :show="success" title="Success!">
+      <h3>A new worksite has been successfully updated!</h3>
+      <template v-slot:buttonText>Go to Worksites' List</template>
+    </Dialog>
+    <worksite-form @action="update"
+                   :old-name="oldData.name"
+                   :old-address="oldData.address"
+                   :old-type="oldData.type"
+    >
+      <template v-slot:title>
+        <h2>Update the worksite</h2>
+      </template>
+      <template v-slot:button>
+        <link-button>Update</link-button>
+      </template>
+    </worksite-form>
+  </div>
 </template>
 
 <script>
@@ -18,6 +24,11 @@ import WorksiteForm from "./WorksiteForm";
 export default {
   name: "UpdateWorksite",
   components:{WorksiteForm},
+  data(){
+    return{
+      success: false
+    }
+  },
   computed:{
     oldData(){
       return this.$store.getters.oldData;
@@ -36,6 +47,9 @@ export default {
       const index = this.$route.path.indexOf('worksite')
       const url = this.$route.path.substring(0, index + 8) + 's';
       this.$router.replace(url);
+    },
+    closeDialog(){
+      this.$router.go(-1);
     }
   }
 }
