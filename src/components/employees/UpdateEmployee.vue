98c3f488cfc1255e33f9ae44656b9a26fdb8da1f
juliadud <yuliiadudkina@gmail.com>
2021-11-26 14:48:28 +0200

    adding success dialog to Update Employee comp

--- src/components/employees/UpdateEmployee.vue
+++ src/components/employees/UpdateEmployee.vue
@@ -1,25 +1,37 @@
 <template>
-  <employee-form @action="update"
-                 :old-full-name="oldEmployee.name"
-                 :old-birth-date="oldEmployee.birthDate"
-                 :old-phone="oldEmployee.phone"
-                 :old-address="oldEmployee.address"
-                 :old-salary="oldEmployee.salary"
-  >
-    <template v-slot:title>
-      <h2>Update an employee</h2>
-    </template>
-    <template v-slot:button>
-      <link-button>Update</link-button>
-    </template>
-  </employee-form>
+  <div>
+    <Dialog @close="closeDialog" :show="success" title="Success!">
+      <h3>The employee has been successfully updated!</h3>
+      <template v-slot:buttonText>Go to Employees' List</template>
+    </Dialog>
+    <employee-form @action="update"
+                   :old-full-name="oldEmployee.name"
+                   :old-birth-date="oldEmployee.birthDate"
+                   :old-phone="oldEmployee.phone"
+                   :old-address="oldEmployee.address"
+                   :old-salary="oldEmployee.salary"
+    >
+      <template v-slot:title>
+        <h2>Update the employee</h2>
+      </template>
+      <template v-slot:button>
+        <link-button>Update</link-button>
+      </template>
+    </employee-form>
+  </div>
 </template>
 
 <script>
 import EmployeeForm from "./EmployeeForm";
+import Dialog from "../UI/Dialog";
 export default {
   name: "AddEmployee",
-  components: {EmployeeForm},
+  components: {EmployeeForm, Dialog},
+  data(){
+    return{
+      success: false
+    }
+  },
   computed:{
     oldEmployee(){
       return this.$store.getters.employee;
@@ -27,10 +39,13 @@ export default {
   },
   methods:{
     async update(data){
+      this.success = true;
       const updatedEmployee = data;
       await this.$store.dispatch('updateEmployee', updatedEmployee);
       await this.$store.dispatch('loadEmployees');
-      await this.$router.replace("/employees");
+    },
+    closeDialog(){
+      this.$router.replace("/employees");
     }
   }
 }
