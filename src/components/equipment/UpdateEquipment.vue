b9e1f7dddb35651bd543a6ed450485136672c4d9
juliadud <yuliiadudkina@gmail.com>
2021-11-26 14:58:06 +0200

    adding success dialog to Update equipment comp

--- src/components/equipment/UpdateEquipment.vue
+++ src/components/equipment/UpdateEquipment.vue
@@ -1,24 +1,36 @@
 <template>
-  <equipment-form @action="update"
-    :old-fee="item.fee"
-    :old-name="item.name"
-    :old-storage="item.storage"
-  >
-    <template v-slot:title>
-      <h2>Update this piece of equipment</h2>
-    </template>
-    <template v-slot:button>
-      <link-button>Update</link-button>
-    </template>
-  </equipment-form>
+  <div>
+    <Dialog @close="closeDialog" :show="success" title="Success!">
+      <h3>This equipment has been successfully updated!</h3>
+      <template v-slot:buttonText>Go to Equipment's List</template>
+    </Dialog>
+    <equipment-form @action="update"
+                    :old-fee="item.fee"
+                    :old-name="item.name"
+                    :old-storage="item.storage"
+    >
+      <template v-slot:title>
+        <h2>Update this piece of equipment</h2>
+      </template>
+      <template v-slot:button>
+        <link-button>Update</link-button>
+      </template>
+    </equipment-form>
+  </div>
 </template>
 
 <script>
 import EquipmentForm from "./EquipmentForm";
+import Dialog from "../UI/Dialog";
 export default {
   name: "AddEquipment",
   components:{
-    EquipmentForm
+    EquipmentForm, Dialog
+  },
+  data(){
+    return{
+      success: false
+    }
   },
   computed:{
     item(){
@@ -27,6 +39,7 @@ export default {
   },
   methods:{
     async update(data){
+      this.success = true;
       const updatedItem = {
         name: data.name,
         fee: data.fee,
@@ -34,7 +47,9 @@ export default {
       }
       await this.$store.dispatch('updateItem',updatedItem);
       await this.$store.dispatch('loadEquipment');
-      await this.$router.replace("/equipment");
+    },
+    closeDialog(){
+      this.$router.replace("/equipment");
     }
   }
 }
